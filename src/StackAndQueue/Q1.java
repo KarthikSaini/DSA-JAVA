bb05d13224280532fb5e9b3556a42d79351e7bc9
Karthik <karthiksaini7800@gmail.com>
2026-08-01 00:51:38 +0530

    Q1 - StackAndQueue - Implement stack using array - Solved

---
+++ src/StackAndQueue/Q1.java
@@ -0,0 +1,26 @@
+package StackAndQueue;
+
+// Implement stack using array
+public class Q1 {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.display();
+//        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+        stack.push(12);
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.isFull());
+        stack.display();
+    }
+}
---
+++ src/StackAndQueue/Stack.java
@@ -0,0 +1,52 @@
+package StackAndQueue;
+
+public class Stack {
+
+    int pointer;
+
+    int[] arr;
+
+    Stack(int size){
+        pointer = -1;
+        arr = new int [size];
+    }
+
+    public void push(int num){
+        if(pointer < arr.length-1){
+            pointer++;
+            arr[pointer] = num;
+        }
+    }
+
+    public Integer pop(){
+        if(pointer > -1){
+            int temp = arr[pointer];
+            pointer--;
+            return temp;
+        }
+        return null;
+    }
+
+    public Integer peek(){
+        if(pointer > -1){
+            return arr[pointer];
+        }
+        return null;
+    }
+
+    public void display(){
+        for(int i=0; i<=pointer; i++){
+            System.out.print(arr[i]+" -> ");
+        }
+
+        System.out.println("Finished");
+    }
+
+    public boolean isEmpty(){
+        return pointer < 0;
+    }
+
+    public boolean isFull(){
+        return pointer >= arr.length-1;
+    }
+}
