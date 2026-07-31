514de9c3baa1e6ecbeca33c87af1dc3cdba6cc3e
Karthik <karthiksaini7800@gmail.com>
2026-07-31 23:37:32 +0530

    Q10 - LinkedList - Add two numbers represented as linked lists - Solved

--- src/LinkedLists/Q10.java
+++ src/LinkedLists/Q10.java
@@ -1,6 +1,7 @@
 package LinkedLists;
 
-// Working but not for leet code and need optimizes as well
+// Working and optimized for leet code
+// Add two numbers represented as linked lists
 public class Q10 {
 
     public static long linkedListToNumber(Node node){
@@ -36,20 +37,94 @@ public class Q10 {
         return result.next;
     }
 
+//    public static Node addTwoNumbers(Node l1, Node l2) {
+//        if(l1.val == 0 && l2.val == 0 && l1.next == null && l2.next == null){
+//            return new Node(0);
+//        }
+//        return numberToLinkedList(linkedListToNumber(l1)+linkedListToNumber(l2));
+//    }
+
+    public static int likedlistLen(Node node){
+        if(node == null){
+            return 0;
+        }
+
+        int len = 0;
+
+        while(node != null){
+            len++;
+            node = node.next;
+        }
+
+        return len;
+    }
+
     public static Node addTwoNumbers(Node l1, Node l2) {
-        if(l1.val == 0 && l2.val == 0 && l1.next == null && l2.next == null){
+        if (l1.val == 0 && l2.val == 0 && l1.next == null && l2.next == null) {
             return new Node(0);
         }
-        return numberToLinkedList(linkedListToNumber(l1)+linkedListToNumber(l2));
+
+
+        Node larger = null;
+        Node temp = new Node(-1);
+        Node result = temp;
+
+        if(likedlistLen(l1) > likedlistLen(l2)){
+            larger = l1;
+        } else {
+            larger = l2;
+        }
+
+        int sum = 0;
+
+        while(larger != null ){
+
+            if(l1 != null){
+//                System.out.println(l1.val);
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null){
+//                System.out.println(l2.val);
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+
+            temp.next = new Node(sum%10);
+            temp = temp.next;
+
+//            if we want answer in reverse order
+//            Node temp = new Node(sum%10);
+//            temp.next = result;
+//            result = temp;
+            sum = sum / 10;
+
+            larger = larger.next;
+        }
+
+        if(sum > 0){
+//            if we want answer in reverse order
+//            Node temp = new Node(sum%10);
+//            temp.next = result;
+//            result = temp;
+
+            temp.next = new Node(sum%10);
+            temp = temp.next;
+
+        }
+
+        return result.next;
     }
 
     public static void main(String[] args) {
 //        int[] number1 = {2,4,3};
 //        int[] number2 = {5,6,4};
-//        int[] number1 = {9,9,9,9,9,9,9};
-//        int[] number2 = {9,9,9,9};
-        int[] number1 = {9};
-        int[] number2 = {1,9,9,9,9,9,9,9,9,9};
+        int[] number1 = {9,9,9,9,9,9,9};
+        int[] number2 = {9,9,9,9};
+//        int[] number1 = {9};
+//        int[] number2 = {1,9,9,9,9,9,9,9,9,9};
+//        int[] number1 = {9,9,9,9};
+//        int[] number2 = {9};
         Node list1 = Node.add(number1);
         Node list2 = Node.add(number2);
 //        Node.display(list1);
