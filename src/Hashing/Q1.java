187bc600025bb3fd627d2f5c9afc5d8857f141e5
Karthik <karthiksaini7800@gmail.com>
2026-08-03 23:55:57 +0530

    Q1 - Hashing - First repeating element - Solved

--- src/Hashing/Q1.java
+++ src/Hashing/Q1.java
@@ -1,5 +1,23 @@
 package Hashing;
 
+import java.util.HashSet;
+
 // First repeating element
 public class Q1 {
+
+    public static int FirstRepeatingElement(int[] input){
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int i : input){
+            if(!hashSet.add(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {1,2,3,4,5,2,1};
+        int result = FirstRepeatingElement(input);
+        System.out.println(result);
+    }
 }
