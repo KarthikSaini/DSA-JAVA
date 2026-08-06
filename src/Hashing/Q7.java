6233e1c9b2a664eac4ae30b04c6ef81f3cc305df
Karthik <karthiksaini7800@gmail.com>
2026-08-06 20:52:09 +0530

    Q7 - Hashing - Subarray sum equals K - Not Solved

--- src/Hashing/Q7.java
+++ src/Hashing/Q7.java
@@ -1,4 +1,31 @@
 package Hashing;
 
 public class Q7 {
+
+    public static int subarraySum(int[] nums, int k) {
+
+        int result = 0;
+
+        if (nums.length == 0) {
+            return result;
+        }
+
+        for(int i=0; i<nums.length; i++){
+            int temp = 0;
+            for(int j=i; j<nums.length; j++){
+                if()
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {1,1,1,2,2,3};
+        int k = 1;
+        int result = subarraySum(input, k);
+        System.out.println(result);
+
+    }
+
 }
