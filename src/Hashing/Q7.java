8a9fee9facdaa159840276ddc3e6a15cf2c64ba4
Karthik <karthiksaini7800@gmail.com>
2026-08-08 03:41:41 +0530

    Q7 - Hashing - Subarray sum equals K - Solved

--- src/Hashing/Q7.java
+++ src/Hashing/Q7.java
@@ -1,28 +1,52 @@
 package Hashing;
 
+import java.util.HashMap;
+
+// Subarray sum equals K
 public class Q7 {
 
     public static int subarraySum(int[] nums, int k) {
 
-        int result = 0;
+        int total_count = 0;
 
         if (nums.length == 0) {
-            return result;
+            return total_count;
         }
 
-        for(int i=0; i<nums.length; i++){
-            int temp = 0;
-            for(int j=i; j<nums.length; j++){
-                if()
+        int current_sum = 0;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        hashMap.put(0,1);
+
+//        for(int num : nums){
+//            current_sum = current_sum + num;
+//
+//            int sum0 = current_sum-k;
+//
+//            if(hashMap.containsKey(sum0)){
+//                total_count = total_count + hashMap.get(sum0);
+//            }
+//
+//            hashMap.put(current_sum, hashMap.getOrDefault(current_sum, 0)+1);
+//        }
+
+        for(int num : nums){
+            current_sum = current_sum + num;
+            int sum0 = current_sum - k;
+
+            if(hashMap.containsKey(sum0)){
+                total_count = total_count + hashMap.get(sum0);
             }
+
+            hashMap.put(current_sum, hashMap.getOrDefault(current_sum, 0)+1);
         }
 
-        return result;
+        return total_count;
     }
 
     public static void main(String[] args) {
-        int[] input = {1,1,1,2,2,3};
-        int k = 1;
+//        int[] input = {2,3,-5,5,-5,1,4};
+        int[] input = {2,3,-5,5};
+        int k = 5;
         int result = subarraySum(input, k);
         System.out.println(result);
 
