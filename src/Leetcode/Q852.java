1c684b26dc5d9cb308a9ecb993e512fd79a59875
Karthik <karthiksaini7800@gmail.com>
2026-08-30 23:45:56 +0530

    Q852 - leetcode - solved

--- src/Leetcode/Q852.java
+++ src/Leetcode/Q852.java
@@ -2,21 +2,40 @@ package Leetcode;
 
 public class Q852 {
 
-    static public int peakIndexInMountainArray(int[] arr) {
-        int max = Integer.MIN_VALUE;
-        int index = -1;
+//    Approach 1
+//    static public int peakIndexInMountainArray(int[] arr) {
+//        int max = Integer.MIN_VALUE;
+//        int index = -1;
+//
+//        for(int i=0; i<arr.length; i++){
+//            if (arr[i] > max){
+//                max = arr[i];
+//                index = i;
+//            }
+//        }
+//        return index;
+//    }
 
-        for(int i=0; i<arr.length; i++){
-            if (arr[i] > max){
-                max = arr[i];
-                index = i;
-            }
-        }
-        return index;
-    }
+//    Approach 2
+//    static public int peakIndexInMountainArray(int[] arr) {
+//        int max = Integer.MIN_VALUE;
+//        int index = -1;
+//
+//        for(int i=0; i<arr.length; i++){
+//            if (arr[i] > max){
+//                max = arr[i];
+//                index = i;
+//
+//                if(arr[i] < max){
+//                    break;
+//                }
+//            }
+//        }
+//        return index;
+//    }
 
     public static void main(String[] args) {
-        int[] input = {0,10,5,2};
+        int[] input = {1, 2, 3, 4, 5, 6, 10, 9, 7, 4, 2};
         System.out.println(peakIndexInMountainArray(input));
     }
 }
