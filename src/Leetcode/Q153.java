f63f12b9514f8eea6d61fa6c441c91fb702693bf
Karthik <karthiksaini7800@gmail.com>
2026-09-01 00:05:02 +0530

    Q153 - leetcode - solved

--- src/Leetcode/Q153.java
+++ src/Leetcode/Q153.java
@@ -11,7 +11,7 @@ public class Q153 {
         while(left < right){
             int mid = (left + right) / 2;
 
-            if(nums[mid] > nums[mid + 1]){
+            if(nums[mid] > nums[right]){
                 left = mid + 1;
             } else {
                 right = mid;
@@ -21,7 +21,7 @@ public class Q153 {
     }
 
     public static void main(String[] args) {
-        int[] input = {4,5,6,7,0,1,2};
+        int[] input = {2,3,4,5,1};
         System.out.println("Result "+findMin(input));
     }
 }
