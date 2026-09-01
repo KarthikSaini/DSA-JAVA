6172285e342da094aca0bc2a39ecd463cee95972
Karthik <karthiksaini7800@gmail.com>
2026-09-01 23:13:19 +0530

    Q540 - leetcode - solved

--- src/Leetcode/Q540.java
+++ src/Leetcode/Q540.java
@@ -1,4 +1,31 @@
 package Leetcode;
 
 public class Q540 {
+
+    static public int singleNonDuplicate(int[] nums) {
+
+        if(nums.length == 1){
+            return nums[0];
+        }
+
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+
+
+        for(int i=0; i<nums.length-1; i = i + 2){
+            if(nums[i] != nums[i+1]){
+
+//                System.out.println("return"+ nums[i] + "   " +nums[i+1]);
+                return nums[i];
+            }
+        }
+
+        return nums[nums.length-1];
+    }
+
+    public static void main(String[] args) {
+        int[] input = {3,3,7,7,10,11,11};
+        System.out.println(singleNonDuplicate(input));
+    }
 }
